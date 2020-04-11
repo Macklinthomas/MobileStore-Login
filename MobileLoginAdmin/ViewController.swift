19a1a908076f9df30398dafd314eb4aa46b1b3c5
Macklin Thomas <Macklinthomas5@gmail.com>
2020-04-11 04:12:24 -0400

    Initial Commit

---
+++ MobileLoginAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileLoginAdmin
+//
+//  Created by Macklin Thomas on 4/11/20.
+//  Copyright © 2020 Macklin Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
